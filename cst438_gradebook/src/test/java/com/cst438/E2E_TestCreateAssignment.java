e9c8d1ea62bf9c03a2fe1532d062c3ec501d52d3
diego <diegovega2249@gmail.com>
2022-11-02 01:26:48 -0700

    tests buttons

--- cst438_gradebook/src/test/java/com/cst438/E2E_TestCreateAssignment.java
+++ cst438_gradebook/src/test/java/com/cst438/E2E_TestCreateAssignment.java
@@ -1,5 +1,53 @@
 package com.cst438;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class E2E_TestCreateAssignment {
 
+	public static final String CHROME_DRIVER_FILE_LOCATION = 
+			"C:/Drivers/selenium/chromedriver.exe";
+	public static final String URL = "http://localhost.com:3000";
+	public static final int SLEEP_DURATION = 1000;
+	
+	@Test
+	public void addAssignment() throws Exception {
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		
+		WebDriver driver = new ChromeDriver();
+		
+		try {
+			WebElement we;
+			
+			driver.get(URL);
+			Thread.sleep(SLEEP_DURATION);
+			
+			we = driver.findElement(By.xpath("//button[@id='addAssignment']"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			we = driver.findElement(By.xpath("//button[@id='add']"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			throw e;
+			
+		} finally {
+			driver.quit();
+		}
+	}
+	
 }
